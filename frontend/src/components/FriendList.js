7a7f6e34d57288e6f47078aa169af2c01cf93633
Snaczeek <mateusz0filoda@gmail.com>
2024-07-03 22:22:22 +0200

    Major css fixes and tweaks
    Fixed all CSS bugs within the main application, and tweaks.

--- frontend/src/components/FriendList.js
+++ frontend/src/components/FriendList.js
@@ -10,10 +10,10 @@ export const FriendList = () => {
     let i = 0
 
   return (
-    <div className='w-[75%] overflow-y-auto scrollbar'>
+    <div className='w-[75%] flex flex-col h-screen'>
       <Link to={"./addFriend"}><div className='add-friend-button'>Add Friend</div></Link>
       <div className='text-center p-4 text-gray-500'>Friends list</div>
-      <div className=''>
+      <div className='flex-grow overflow-y-auto scrollbar'>
         {friends.map(f => (
             <Link to={"./mcon/"+f} ><div key={i++}className='friend-container'>{f}</div></Link>
         ))}
